50c88f133ddf1a92255121cd9c6bbf96d9828f5b
vfeng <823059782@qq.com>
2020-05-23 23:01:47 +0800

    toast android

---
+++ android/main/java/com/rn/testmoudle/toast/CustomToastPackage.java
@@ -0,0 +1,33 @@
+package com.rn.testmoudle.toast;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Created by vfeng on 2020/5/23.
+ */
+public class CustomToastPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new NewToastModule(reactContext));
+
+        return modules;
+    }
+
+}

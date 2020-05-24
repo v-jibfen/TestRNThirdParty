86ba8474dab59031b61ef1ab79756bc3984e082f
vfeng <823059782@qq.com>
2020-05-24 12:58:20 +0800

    测试rn 自定义方法

--- src/NewToast.android.js
+++ src/NewToast.android.js
@@ -1,4 +1,10 @@
 'use strict';
 
 import {NativeModules} from 'react-native';
-export default NativeModules.NewToastAndroid
+// export default NativeModules.NewToastAndroid
+
+var Toast = NativeModules.NewToastAndroid;
+
+function show1(message, duration) {
+    Toast.show(message, duration)
+}

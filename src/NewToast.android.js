799127b286235d6c67edd30b2c857990e47469dc
vfeng <823059782@qq.com>
2020-05-24 14:02:52 +0800

    封装rn js 调原生api

--- src/NewToast.android.js
+++ src/NewToast.android.js
@@ -1,10 +1,30 @@
 'use strict';
 
 import {NativeModules} from 'react-native';
+var Toast = NativeModules.NewToastAndroid;
 // export default NativeModules.NewToastAndroid
 
-var Toast = NativeModules.NewToastAndroid;
+const CreateAndroid = {
 
-function show1(message, duration) {
-    Toast.show(message, duration)
-}
+  // Toast duration constants
+  SHORT: (Toast.getConstants().SHORT: number),
+  LONG: (Toast.getConstants().LONG: number),
+  // Toast gravity constants
+  TOP: (Toast.getConstants().TOP: number),
+  BOTTOM: (Toast.getConstants().BOTTOM: number),
+  CENTER: (Toast.getConstants().CENTER: number),
+
+  show: function(message: string, duration: number): void {
+    Toast.show(message, duration);
+  },
+
+  showWithGravity: function(
+    message: string,
+    duration: number,
+    gravity: number,
+  ): void {
+    Toast.showWithGravity(message, duration, gravity);
+  },
+};
+
+module.exports = CreateAndroid;
